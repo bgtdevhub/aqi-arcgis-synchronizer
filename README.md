7cfbe5ee98a0adf4280514d6a67435d0483f6eee
nasrulmuhaimin <nzain@bousteadgeospatial.com>
2020-01-17 15:54:39 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # aqi-arcgis-synchronizer
-Lambda function that synchronize EPA data to ArcGIS layer data
+
+AWS Lambda that synchronize EPA AirWatch Air Quality Data to ArcGIS
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+The app can be deploy as standalone NodeJS web app or as a AWS lambda function. To deploy to AWS Lambda, use [Serverless](https://serverless.com/)
+
+### Installing
+
+```
+npm install
+```
+
+### Credentials and Registering your App
+
+For this code to work, you need to
+[add](http://doc.arcgis.com/en/marketplace/provider/add-item-to-agol.htm) and
+[register](http://doc.arcgis.com/en/marketplace/provider/register-app.htm) an app in ArcGIS Online,
+add the correct redirect URI (e.g. `http://localhost:8080`), and add the client ID and secret to [/routes/routes.js](/routes/routes.js).
+
+- [How to add an app in ArcGIS Online](http://doc.arcgis.com/en/marketplace/provider/add-item-to-agol.htm)
+- [How to register an app in ArcGIS Online](http://doc.arcgis.com/en/marketplace/provider/register-app.htm)
+- Make sure to set the correct redirect URI (e.g. `http://localhost:8080`)
+
+Finally, update [/routes/routes.js](/routes/routes.js) to contain your client ID and secret (and portal URL if not ArcGIS Online):
+
+```javascript
+const client_id = 'xx';
+const client_secret = 'xx';
+```
+
+Update the feature server URL
+
+```javascript
+const featureServerUrl = 'xx';
+```
+
+### About API we call
+
+We're calling below API:
+Request URL: https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites/{siteId}
+Description: Provide the details of a single environment monitoring site with current health advice. It only provides last 48 hours of data.
+
+all API called required us to provide API key, make sure you register one in https://portal.api.epa.vic.gov.au/.  
+
+Then, in [/routes/routes.js](/routes/routes.js) assign that API key to the variable
+
+```javascript
+const apiKey = 'xx';
+
+
+### Deploy to AWS Lambda
+
+```
+sls deploy
+```
+
+### Running the Lambda function offline
+
+```
+sls offline start
+```
+
+### Trigger AWS Lambda function
+```
+sls invoke -f app
+```
+
+## To run as a NodeJS app
+
+Comment out the Serverless handle and uncomment the local server part in [/index.js](/index.js)
+
+```javascript
+// module.exports.handler = serverless(app);
+
+// USE THIS FOR LOCAL SERVER
+var server = app.listen(3000, function() {
+  console.log('app running on port.', server.address().port);
+});
+```
+
+## Authors
+
+- **Nasrul Muhaimin b Mohd Zain** - _Initial work_
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ routes/routes.js
@@ -0,0 +1,160 @@
+const request = require('request');
+
+const client_id = '';
+const client_secret = '';
+const apiKey = '';
+const featureServerUrl = '';
+const featureServerUrlApplyEdit = `${featureServerUrl}/applyEdits`;
+const oauth2Url = 'https://www.arcgis.com/sharing/rest/oauth2/token/';
+
+const getFeatureData = (token) =>
+  new Promise((resolve, reject) => {
+    request(
+      {
+        url: `${featureServerUrl}/query?token=${token}&where=1%3D1&f=json&outFields=OBJECTID, siteID`,
+        headers: {},
+        method: 'GET',
+        encoding: null
+      },
+      function (error, res, body) {
+        if (res.statusCode == 200 && !error) {
+          resolve(JSON.parse(body));
+        }
+        reject(error);
+      }
+    );
+  });
+
+const getSourceData = siteId =>
+  new Promise((resolve, reject) => {
+    const sourceUrl = `https://gateway.api.epa.vic.gov.au/environmentMonitoring/v1/sites/${siteId}`
+    request(
+      {
+        url: sourceUrl,
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': apiKey,
+        },
+        method: 'GET',
+        encoding: null
+      },
+      function (error, res, body) {
+        if (res.statusCode == 200 && !error) {
+          resolve(JSON.parse(body));
+        }
+        reject(error);
+      }
+    );
+  });
+
+const applyEdit = (updateData, token) =>
+  new Promise((resolve, reject) => {
+    const updates = updateData.map(data => {
+      return {
+        attributes: {
+          OBJECTID: data.objectId,
+          siteName: data.siteName,
+          siteType: data.siteType,
+          since: data.since,
+          until: data.until,
+          healthParameter: data.healthParameter,
+          healthAdvice: data.healthAdvice,
+          averageValue: data.averageValue
+        }
+      };
+    });
+    console.log(`updates: ${JSON.stringify(updates)}`);
+
+    request.post(
+      {
+        url: featureServerUrlApplyEdit,
+        json: true,
+        formData: {
+          updates: JSON.stringify(updates),
+          f: 'json',
+          token: token
+        }
+      },
+      function (error, response, body) {
+        if (response.statusCode == 200 && !error) {
+          resolve(body);
+        }
+        reject(error);
+      }
+    );
+  });
+
+
+const requestToken = () =>
+  // generate a token with client id and client secret
+  new Promise((resolve, reject) => {
+    request.post(
+      {
+        url: oauth2Url,
+        json: true,
+        form: {
+          f: 'json',
+          client_id,
+          client_secret,
+          grant_type: 'client_credentials',
+          expiration: '1440'
+        }
+      },
+      function (error, response, { access_token }) {
+        if (error) reject(error);
+
+        resolve(access_token);
+      }
+    );
+  });
+
+
+const appRouter = app => {
+
+  app.get('/', async (req, res) => {
+
+    console.log("Synchronization started.")
+    try {
+
+      //1. Request tokens from ArcGIS online
+      const token = await requestToken();
+
+      //2. Get feature data
+      const featureData = await getFeatureData(token);
+
+      let arrUpdates = [];
+
+      for (let index = 0; index < featureData.features.length; index++) {
+
+        const objectId = featureData.features[index].attributes.OBJECTID;
+        const siteID = featureData.features[index].attributes.siteID;
+
+        const sourcedata = await getSourceData(siteID);
+        const updatedData = {
+          objectId: objectId,
+          siteID: sourcedata.siteID,
+          siteName: sourcedata.siteName,
+          siteType: sourcedata.siteType,
+          since: sourcedata.siteHealthAdvices[0].since,
+          until: sourcedata.siteHealthAdvices[0].until,
+          healthParameter: sourcedata.siteHealthAdvices[0].healthParameter,
+          healthAdvice: sourcedata.siteHealthAdvices[0].healthAdvice,
+          averageValue: sourcedata.siteHealthAdvices[0].averageValue
+        };
+        arrUpdates.push(updatedData);
+      }
+
+      const result = await applyEdit(arrUpdates, token);
+      res
+        .status(200)
+        .send(res.body);
+      return;
+    } catch (e) {
+      console.log(e);
+    }
+
+    console.log("Synchronization completed.")
+  });
+};
+
+module.exports = appRouter;
